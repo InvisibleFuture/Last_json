161a2b6434cef6df22b9c2c271d1d4a2d66ee50c
satori <huan0016@gmail.com>
2022-04-02 06:12:19 +0800

    DEBUG

--- hook/user_index_end.php
+++ hook/user_index_end.php
@@ -6,25 +6,36 @@ empty($_user) AND message(-1, "用户不存在");
 // 执行对应操作模式
 switch ($method) {
   case 'POST':
-    // 向此用户添加什么..? 上传头像?
+    // 向此用户添加什么或上传头像
+    message(0, 'POST');
     break;
   case 'PATCH':
+    // 获取 JSON 数据
+    $data = json_decode(file_get_contents("php://input"));
+
     // 如果修改密码
-    $password = param('password');
-    if(!empty(password)) {
+    if (isset($data->password)) {
       $salt = $_user['salt'];
-      $password = md5($password.$salt);
+      $password = md5($data->password.$salt);
       user_update($_uid, array('password'=>$password));
       !is_password($password, $err) AND message('password', $err);
-      message(0, lang('modify_successfully'));
+    }
+    
+    // 如果修改用户名(格式刷 html转码, sql转码)
+    if (isset($data->username)) {
+      user_update($_uid, array('username'=>$data->username));
     }
 
-    // 如果修改用户名
-    // 如果修改签名
+    // 如果修改签名(格式刷 html转码, sql转码)
+    if (isset($data->emotional)) {
+      user_update($_uid, array('emotional'=>$data->emotional));
+    }
+    
+    message(0, '修改成功');
     break;
-  default:
-    // 是 GET, 向下放行
 }
+
+// 放行 GET
 
 ?>
 
